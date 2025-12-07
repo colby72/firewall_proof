40ac0b57c67676abd12d40911317d9ba2c375940
“colby72” <junk@mail.com>
2025-12-07 15:08:26 +0100

    settings layout

--- gui/edit/settings.py
+++ gui/edit/settings.py
@@ -2,13 +2,13 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 
 
-class Settings(QWidget):
+class Settings(QDialog):
     def __init__(self, main_window):
         super().__init__()
         self.main_window = main_window
         self.setWindowTitle(f"Settings - Firewall Proof {self.main_window.version}")
         self.setWindowIcon(QIcon("img/screwdriver_wrench_icon.png"))
-        #self.resize(410, 210)
+        self.resize(400, 410)
 
         # widget design
         layout = QVBoxLayout()
@@ -32,6 +32,7 @@ class Settings(QWidget):
         self.hosts_per_row.setRange(3, 7)
         self.hosts_per_row.setValue(5)
         general_layout.addWidget(self.hosts_per_row, 1, 1)
+        general_layout.setRowStretch(general_layout.rowCount(), 1)
 
         # report settings tab
         report_tab = QWidget()
@@ -44,10 +45,11 @@ class Settings(QWidget):
         self.default_report_format.addItems(["Latex", "HTML", "Docx"])
         self.default_report_format.setCurrentText(self.main_window.report_format)
         report_layout.addWidget(self.default_report_format, 0, 1)
+        report_layout.setRowStretch(report_layout.rowCount(), 1)
 
         # add tabs to global layout and buttons
         tabs.addTab(general_tab, "General")
-        tabs.addTab(report_tab, "Report")
+        tabs.addTab(report_tab, "Reporting")
 
         self.buttons = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
         self.buttons.accepted.connect(self.when_ok)
